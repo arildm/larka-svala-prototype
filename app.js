b85c46c47aafed574cacf5f959a8bd607070cea0
Arild Matsson <arild.matsson@gu.se>
2021-06-15 16:47:34 +0200

    Cleanup

--- app.js
+++ app.js
@@ -4,41 +4,44 @@ const appUrl = 'http://demo.spraakdata.gu.se/arild/larkadb/app.html'
 const essayId = 'Q11QT1'
 let graph = null
 
+/** Save essay and open it with Svala */
 async function gotoSvala() {
     if (!graph) saveEssay();
     console.log('leaving for Svala');
     window.location = `${svalaUrl}#backurl=${btoa(appUrl)}&backend=${btoa(dbUrl)}&essay=${essayId}&start_mode=anonymization`
 }
 
-async function saveEssay() {
-    const essayText = document.getElementsById('text')[0].value
+/** Save essay input to backend. */
+const saveEssay = () => postGraph(textToGraph(readText()));
+
+/** Read text input. */
+const readText = () => document.getElementById('text').value
+
+/** Post essay graph to backend. */
+async function postGraph(graph) {
     const saveResponse = await fetch(`${dbUrl}${essayId}`, {
         method: 'POST',
-        body: JSON.stringify(textToSvala(essayText)),
+        body: JSON.stringify(graph),
     })
     console.log('saved', await saveResponse.json());
 }
 
-function textToSvala(text) {
-    const g = {
-        source: [],
-        target: [],
-        edges: {},
-    }
-    const tokens = text.trim().split(/\s|(?=[,\.!?])/)
-    tokens.forEach((token, i) => {
+/** Tokenize the way Svala wants it. */
+const tokenize = (text) => text.trim().split(/\s|(?=[,\.!?])/)
+
+/** Create graph from text. */
+const textToGraph = (text) => tokenize(text)
+    .reduce((g, token, i) => {
         g.source.push({ id: `s${i}`, text: `${token} ` })
         g.target.push({ id: `t${i}`, text: `${token} ` })
-        g.edges[`e-s${i}-t${i}`] = {
-            id: `e-s${i}-t${i}`,
-            ids: [`s${i}`, `t${i}`],
-            labels: [],
-            manual: false,
-        }
-    })
-    return g
-}
+        g.edges[`e-s${i}-t${i}`] = { id: `e-s${i}-t${i}`, ids: [`s${i}`, `t${i}`], labels: [], manual: false }
+        return g;
+    }, graphInit())
 
+/** Create a new, empty graph. */
+const graphInit = () => ({source: [], target: [], edges: {}})
+
+/** Fetch graph from backend. */
 async function fetchGraph() {
     const response = await fetch(`${dbUrl}${essayId}`)
     const data = await response.json()
@@ -46,14 +49,20 @@ async function fetchGraph() {
     if (!data.error) {
         graph = JSON.parse(data.state)
         console.log('graph', graph);
-        document.getElementById('svala').style.display = 'block';
-        document.getElementById('text').innerHTML = graph.source.map(token => token.text).join('')
-        document.getElementById('anon').innerHTML = graph.target.map(token => token.text).join('')
-        document.getElementById('text').setAttribute('disabled', 'disabled');
+        loadGraph(graph)
     }
 }
 fetchGraph();
 
+/** Load graph into GUI. */
+function loadGraph(graph) {
+    document.getElementById('svala').style.display = 'block';
+    document.getElementById('text').innerHTML = graph.source.map(token => token.text).join('')
+    document.getElementById('anon').innerHTML = graph.target.map(token => token.text).join('')
+    document.getElementById('text').setAttribute('disabled', 'disabled');
+}
+
+/** Delete remote essay and reload page. */
 async function reset() {
     const response = await fetch(`${dbUrl}${essayId}`, {
         method: 'DELETE',
--- index.php
+++ index.php
@@ -1,53 +1,59 @@
 <?php
+/**
+ * @file A very simple "database" implementation.
+ */
+
+// Get parameters from path
 $path = str_replace('arild/larkadb/', '', $_SERVER['REQUEST_URI']);
 $args = array_filter(explode('/', trim($path, '/')));
 $method = $_SERVER['REQUEST_METHOD'];
 
+// Determine which endpoint/method is being used.
 switch (count($args)) {
+    case 1: // Path: /:key
+        if ($method == 'POST'):
+            // Save essay graph.
+            $data = file_get_contents("php://input");
+            respond(save_version($args[0], $data, 1));
+        elseif ($method == 'DELETE'):
+            // Delete essay.
+            // Only for the prototype. In the real SweLL portal, deletion happens only within Django.
+            respond(delete_essay($args[0]));
+        else:
+            // Get essay graph.
+            respond(get_essay($args[0]));
+        endif;
+        break;
+    case 2: // Path: /:key/status
+        // Get status.
+        respond(get_status($args[0]));
+        break;
+    case 3: // Path: /:key/version/:version (should be POST)
+        // Auto-save new version of the essay.
+        $data = file_get_contents("php://input");
+        respond(save_version($args[0], $data, (int) $args[2]));
+        break;
     case 0:
         // Probably the pseudonymizer service proxy.
         // TODO Doesn't work, need to login etc.
         respond_error('Cannot use pseudonymization service in the prototype.', 406);
-        // $data = file_get_contents("php://input");
-        // $pseuws_url = 'https://spraakbanken.gu.se/swell/portal/annotation/pseuws';
-        // $response = post_request($pseuws_url, $data);
-        // respond(json_decode($response));
         break;
-    case 1:
-        if ($method == 'POST'):
-            // POST :key
-            $data = file_get_contents("php://input");
-            respond(save_version($args[0], $data, 1));
-        elseif ($method == 'DELETE'):
-            respond(delete_essay($args[0]));
-        else:
-            // GET :key
-            respond(get_essay($args[0]));
-        endif;
-        break;
-    case 2:
-        // GET :key/status
-        respond(get_status($args[0]));
-        break;
-    case 3:
-        // POST :key/version/:version
-        $data = file_get_contents("php://input");
-        respond(save_version($args[0], $data, (int) $args[2]));
-        break;
-}
+    }
 
+/** Send JSON response. */
 function respond($data) {
     header('Content-Type: application/json');
     echo json_encode($data);
 }
 
+/** Send error response. */
 function respond_error($reason, $code=500) {
     http_response_code($code);
     respond(['error' => $reason]);
 }
 
+/** Load the essay file. */
 function get_essay($key) {
-    // Open the edited file if it exists, otherwise the default one.
     $json = file_get_contents("essays/$key.json");
     if (!$json)
         return [ 'error' => "No essay named '$key'" ];
@@ -59,10 +65,12 @@ function get_essay($key) {
     ];
 }
 
+/** Get status (fake, never "done"). */
 function get_status($key) {
     return ['done' => FALSE];
 }
 
+/** Save essay to file (skips version check). */
 function save_version($key, $data, $version) {
     $result = file_put_contents("essays/$key.json", $data);
     return $result !== FALSE
@@ -70,19 +78,8 @@ function save_version($key, $data, $version) {
         : ['error' => 'Could not save'];
 }
 
+/** Delete essay file. */
 function delete_essay($key) {
     $result = unlink("essays/$key.json");
     return $result ? ['deleted' => $key] : ['error' => 'Could not delete'];
-}
-
-function post_request($url, $data) {
-    $options = [
-        'http' => [
-            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-            'method'  => 'POST',
-            'content' => http_build_query($data),
-        ],
-    ];
-    $context = stream_context_create($options);
-    return file_get_contents($url, false, $context);
 }
